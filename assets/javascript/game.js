1e1f104d4fe28c5be57bdaab36ae402dce9afa7f
sully34 <sssulai@gmail.com>
2018-06-28 18:12:03 -0400

    added audio file

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var name = prompt ("player 1, enter your name."); 
+var word = prompt("player 1, enter your word.");
+var i;
+for (i = 0; index < name.length; i++) {
+    const name = array[index];
+    
+}
+var name = prompt("player 2, enter your name.");
+var secret = []
+var step;
+for (word = 0; word < 2; word++)
+console.log()"Player 1's name is"
