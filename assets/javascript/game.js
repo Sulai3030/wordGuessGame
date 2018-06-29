1e36fb07b9436f5f1ad8e27edf6df84d69d9a1a7
sully34 <sssulai@gmail.com>
2018-06-29 16:39:36 -0400

    new background image uploaded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,77 +17,9 @@
               audioElement.pause();
             });
         
-            // Size Buttons
-            $(".normal-button").on("click", function() {
-              captainPlanet.animate({ height: "300px" });
-            });
-            $(".grow-button").on("click", function() {
-              captainPlanet.animate({ height: "500px" });
-            });
-            $(".shrink-button").on("click", function() {
-              captainPlanet.animate({ height: "100px" });
-            });
+
         
-            $(".stretch-btn").on("click",function(){
-            captainPlanet.animate({height:"100%"})
-            
-            })
-        
-            // Visibility Buttons
-            $(".vis-button").on("click", function() {
-              captainPlanet.animate({ opacity: "1" });
-            });
-            $(".invis-button").on("click", function() {
-              captainPlanet.animate({ opacity: "0.05" });
-            });
-        
-            // Move Buttons
-            $(".up-button").on("click", function() {
-              captainPlanet.animate({ top: "-=200px" }, "normal");
-            });
-            $(".down-button").on("click", function() {
-              captainPlanet.animate({ top: "+=200px" }, "normal");
-            });
-            $(".left-button").on("click", function() {
-              captainPlanet.animate({ left: "-=200px" }, "normal");
-            });
-            $(".right-button").on("click", function() {
-              captainPlanet.animate({ left: "+=200px" }, "normal");
-            });
-            $(".back-button").on("click", function() {
-              captainPlanet.animate({ top: "50px", left: "80px" }, "fast");
-            });
-        
-            // Keyboard move controls
-            $(document).keyup(function(e) {
-              switch (e.which) {
-        
-              // Move Buttons (Keyboard Down)
-              case 40:
-                captainPlanet.animate({ top: "+=200px" }, "normal");
-                break;
-        
-                // Move Buttons (Keyboard Right)
-              case 39:
-                captainPlanet.animate({ left: "+=200px" }, "normal");
-                break;
-        
-                // Move Buttons (Keyboard Up)
-              case 38:
-                captainPlanet.animate({ top: "-=200px" }, "normal");
-                break;
-        
-                // Move Buttons (Keyboard Left)
-              case 37:
-                captainPlanet.animate({ left: "-=200px" }, "normal");
-                break;
-        
-              default:
-                break;
-              }
-            });
-          });
-        
+          
           </script>var name = prompt ("player 1, enter your name."); 
 var word = prompt("player 1, enter your word.");
 var i;
