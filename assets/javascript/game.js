3192053050c1cfefe37410e393f97922a43eba97
sully34 <sssulai@gmail.com>
2018-06-30 18:30:35 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,33 @@
-var name = prompt ("player 1, enter your name."); 
-var word = prompt("player 1, enter your word.");
-var i;
-
-function resetGame(){
-    resetUI();
+function resetGame()
+{
+    resetGame();
     gameAnswer = chooseWord();
     gameShownAnswer = blanksFromAnswer(gameAnswer);
     hangmanState = 0;
     drawWord(gameShownAnswer);
 }
-$(document).ready(resetGame);
-function win () {alert('Yay! You Win!')
-}; resetGame();
-function lose () {alert('Ooof! You lose')
-};resetGame();
-function keypress(){
-    var tempChar = $('#letterInput').val("");
+var name = prompt ["player 1, enter your name."]; 
+var word = prompt["player 1, enter your word."];
+var bandNames = ['Nirvana', 'Pearl Jam', 'AliceinChains', 'Mudhoney']; 
+var bandNames = prompt["What's you favorite 90's band?"]
+ var first = bandNames[0];
+var last = bandNames[bandNames-1];
+bandNames.forEach(function)item, index, array){
+    console.log
 }
-if ()
+bandNames.push();
+var pos = bandNames.indexOf()
+
+if(score = )
+
+
+
+
+//generate an array of band names
+//get it 
+
+
+//generate an array of band names ie ["Backstreet Boys", "Green Day", "U2"]
+//pick one of the bands from the array
+//make it display on the page
+//after making it display on the page, how can you make these appear as
