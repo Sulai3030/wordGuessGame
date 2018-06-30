8dc06ee4cab309173c272e97dbd4cbdb301eff1f
sully34 <sssulai@gmail.com>
2018-06-29 22:31:57 -0400

    updated js css html files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,20 @@
-<script>
-        
-          // JavaScript function that wraps everything
-          $(document).ready(function() {
-        
-            var captainPlanet = $(".captain-planet");
-        
-            // Gets Link for Theme Song
-            var audioElement = document.createElement("audio");
-            audioElement.setAttribute("src", "assets/audio/floyedthebarber.mp3");
-        
-            // Theme Button
-            $(".theme-button").on("click", function() {
-              audioElement.play();
-            });
-            $(".pause-button").on("click", function() {
-              audioElement.pause();
-            });
-        
-
-        
-          
-          </script>var name = prompt ("player 1, enter your name."); 
+var name = prompt ("player 1, enter your name."); 
 var word = prompt("player 1, enter your word.");
 var i;
-for (i = 0; index < name.length; i++) {
-    const name = array[index];
-    
+
+function resetGame(){
+    resetUI();
+    gameAnswer = chooseWord();
+    gameShownAnswer = blanksFromAnswer(gameAnswer);
+    hangmanState = 0;
+    drawWord(gameShownAnswer);
 }
-var name = prompt("player 2, enter your name.");
-var secret = []
-var step;
-for (word = 0; word < 2; word++)
-console.log()"Player 1's name is"
-var audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", "assets/audio/floydthebarber.mp3");
-    
+$(document).ready(resetGame);
+function win () {alert('Yay! You Win!')
+}; resetGame();
+function lose () {alert('Ooof! You lose')
+};resetGame();
+function keypress(){
+    var tempChar = $('#letterInput').val("");
+}
+if ()
